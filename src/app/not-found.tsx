364b6a8c8afff535a19e817d8d2b54a0b9c8ef34
hyun907 <seunghyun020907@naver.com>
2024-12-20 17:37:58 +0900

    chore: add not-found page.tsx

---
+++ src/app/not-found.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import style from "./not-found.module.css";
+import IC_RIGHT from "@/assets/svgs/right_pick.svg";
+useRouter;
+
+export default function NotFound() {
+  const router = useRouter();
+  const onClick = () => {
+    router.push("/");
+  };
+
+  return (
+    <div className={style.container}>
+      <div className={style.title}>
+        죄송합니다. 해당 페이지를 찾을 수 없습니다.
+      </div>
+      <div className={style.description}>지금 진행 중인 행사를 구경해봐요!</div>
+      <div className={style.row}>
+        <IC_RIGHT fill={"red"} />
+        <button className={style.btn} onClick={onClick}>
+          HOME
+        </button>
+      </div>
+    </div>
+  );
+}
