1fa237f12bd1db99f637a15ec3287ff0cc2e82ab
hyun907 <seunghyun020907@naver.com>
2025-02-26 18:15:10 +0900

    feat: 비로그인 상태 홈 화면 퍼블리싱

--- src/components/Home/NonLogin.tsx
+++ src/components/Home/NonLogin.tsx
@@ -1,5 +1,6 @@
-import Image from "next/image";
 import styles from "./NonLogin.module.css";
+import Pick_IC from "@/../public/svgs/home/pick_primary.svg";
+import Next_IC from "@/../public/svgs/next_arrow.svg";
 
 export default function NonLogin() {
   return (
@@ -12,9 +13,12 @@ export default function NonLogin() {
           컬픽이 당신을 위한 사용자 맞춤형 추천을 해드립니다.
         </div>
       </div>
-      <div>
-        {/* <Image /> */}
-        <div>로그인하러 가기</div>
+      <div className={styles.loginContainer}>
+        <Pick_IC />
+        <button className={styles.loginBtn}>
+          로그인하러 가기
+          <Next_IC />
+        </button>
       </div>
     </div>
   );
