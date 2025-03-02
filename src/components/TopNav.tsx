9a4ec205bb61de581df0fe3b06ab5accf832ecb8
hyun907 <seunghyun020907@naver.com>
2025-03-02 21:36:33 +0900

    fix: remove map, calendar in nav bar and pages

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -14,10 +14,6 @@ export default function TopNav() {
   useEffect(() => {
     if (pathname === "/") {
       setActiveButton("home");
-    } else if (pathname.startsWith("/map")) {
-      setActiveButton("map");
-    } else if (pathname.startsWith("/calendar")) {
-      setActiveButton("calendar");
     } else if (pathname.startsWith("/picklist")) {
       setActiveButton("picklist");
     } else if (pathname.startsWith("/login")) {
@@ -30,36 +26,11 @@ export default function TopNav() {
   return (
     <nav className={`${styles.nav} nav_pixel_text_22`}>
       <div className={styles.left}>
-        <div>
-          <IC_SYMBOL />
-        </div>
         <div
-          className={`${styles.btn} nav_pixel_text_22 ${
-            activeButton === "home" ? "nav_active" : ""
-          }`}
+          className={styles.btn}
           onClick={() => handleNavigation(router, setActiveButton, "home", "/")}
         >
-          Home
-        </div>
-        <div
-          className={`${styles.btn} nav_pixel_text_22 ${
-            activeButton === "map" ? "nav_active" : ""
-          }`}
-          onClick={() =>
-            handleNavigation(router, setActiveButton, "map", "/map")
-          }
-        >
-          Map
-        </div>
-        <div
-          className={`${styles.btn} nav_pixel_text_22 ${
-            activeButton === "calendar" ? "nav_active" : ""
-          }`}
-          onClick={() =>
-            handleNavigation(router, setActiveButton, "calendar", "/calendar")
-          }
-        >
-          Calendar
+          <IC_SYMBOL />
         </div>
       </div>
       <div className={styles.right}>
