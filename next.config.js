8059f2a47e5e6d058db2c1039676dc1ffc8571b1
hyun907 <seunghyun020907@naver.com>
2025-03-08 23:23:21 +0900

    fix: Node.js의 메모리 제한을 설정하는 옵션 추가

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   webpack: (config) => {
     config.module?.rules?.push({
       test: /\.svg$/,
@@ -8,3 +9,5 @@ module.exports = {
     return config;
   },
 };
+
+module.exports = nextConfig;
