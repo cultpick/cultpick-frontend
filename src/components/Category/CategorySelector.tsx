f091754fa1845c0f009ab5fedb3e30ca1c56d4b0
hyun907 <seunghyun020907@naver.com>
2025-03-12 15:58:05 +0900

    fix: 카테고리 선택 로직 및 스타일링 수정

--- src/components/Category/CategorySelector.tsx
+++ src/components/Category/CategorySelector.tsx
@@ -4,10 +4,21 @@ import { CATEGORIES } from "@/store/categoryState";
 import { useCategorySelection } from "@/hooks/useCategorySelection";
 import styles from "./CategorySelector.module.css";
 import { CategoryIcon } from "./CategoryIcon";
+import { useRouter } from "next/navigation";
 
 export default function CategorySelector() {
+  const router = useRouter();
   const { selectedCategories, toggleCategory, isSelected, canSelectMore } =
     useCategorySelection();
+
+  const handleComplete = () => {
+    // TODO: 선택된 카테고리 저장 로직
+    router.push("/");
+  };
+
+  const handleSkip = () => {
+    router.push("/");
+  };
 
   return (
     <div className={styles.container}>
@@ -25,6 +36,18 @@ export default function CategorySelector() {
             <div className={styles.categoryText}>{category}</div>
           </button>
         ))}
+      </div>
+      <div className={styles.buttonContainer}>
+        <button
+          className={styles.completeButton}
+          onClick={handleComplete}
+          disabled={selectedCategories.length === 0}
+        >
+          선택 완료
+        </button>
+        <button className={styles.skipButton} onClick={handleSkip}>
+          건너뛰기
+        </button>
       </div>
     </div>
   );
